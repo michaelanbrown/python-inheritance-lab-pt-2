822e28cc7be6638878d9a852a78fcb2bfcaea6f0
michaelanbrown <m_brownbrown@yahoo.com>
2024-09-26 16:39:31 -0400

    also printing the extra statement

--- lib/student.py
+++ lib/student.py
@@ -8,3 +8,4 @@ class Student:
 class ChattyStudent(Student):
     def hello(self):
         super().hello()
+        print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
